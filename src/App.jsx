5ce7d9fa77dcf773e661f23fc37a92ed542b955a
Marce <marcelobaltazarpalma@gmail.com>
2025-03-24 13:43:44 -0300

    change import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import confetti from 'canvas-confetti'
 
-import { Square } from './components/Square.jsx'
+import { Square } from './components/square.jsx'
 import { TURNS } from './logic/constants.js'
 import { checkWinnerFrom, checkEndGame } from './components/logic/board.js'
 import { WinnerModal } from './components/winnermodal.jsx'
